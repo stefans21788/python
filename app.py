b31e6ca760d7987219699c572ce6d65c2444e7e9
stefans21788 <39769419+stefans21788@users.noreply.github.com>
2023-07-10 19:11:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def greet():
     name = input("Enter your name: ")
-    print("Hello, " + name + "! Welcome to the Python app.")
+    return "Hello, " + name + "! Welcome to the Python app."
 
 if __name__ == "__main__":
-    greet()
+    app.run()
